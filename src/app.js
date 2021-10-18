43d3fd503512b316d34957f104795da0a1e72387
Melike Nur Şahin <84036682+Melikeshn@users.noreply.github.com>
2021-10-19 00:04:22 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log("Merhaba kodlama io")
+let dolarBugun = 9.30
+
+let dolarDun =9.20
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+
+//const sabit tanımlamakiçin kullanılır.
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+// array
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length; i++) {
+    console.log("<li>"+i+"</li>")
+    
+}
+console.log("</ul>")
+
+
+console.log(konutKredileri)
+
